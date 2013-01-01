e307b5dc7f1071184beb97b8284965dcc568ce7f
Will Sember <semberwj@gmail.com>
2012-12-31 23:15:23 -0600

    added a C program--which will be a child process of Python--that displays a calibration screen

---
+++ calibrate.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <math.h>
+
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int main(int argc, char **argv) {
+	if (!al_install_system(ALLEGRO_VERSION_INT, NULL)) {
+		printf("could not init Allegro\n");
+		return 1;
+	}
+
+	al_set_new_display_flags(ALLEGRO_FULLSCREEN);
+	ALLEGRO_DISPLAY *display = al_create_display(1920, 1080);
+
+	if (!display) {
+		printf("could not create display\n");
+		return 1;
+	}
+
+	if (!al_install_mouse()) {
+		printf("could not install mouse\n");
+		return 1;
+	}
+
+	if (!al_install_keyboard()) {
+		printf("could not install keybaord\n");
+		return 1;
+	}
+
+	if (!al_init_image_addon()) {
+		printf("could not init image addon\n");
+		return 1;
+	}
+
+	ALLEGRO_BITMAP *target = al_load_bitmap("target.jpg");
+
+	al_set_target_backbuffer(display);
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+	al_draw_bitmap(target, 0, 0, 0);
+	al_flip_display();
+
+	ALLEGRO_MOUSE_STATE state;
+
+	ALLEGRO_EVENT_QUEUE *queue = al_create_event_queue();
+	ALLEGRO_EVENT event;
+
+	al_register_event_source(queue, al_get_keyboard_event_source());
+	al_register_event_source(queue, al_get_display_event_source(display));
+
+	int w = al_get_bitmap_width(al_get_backbuffer(display));
+	int h = al_get_bitmap_height(al_get_backbuffer(display));
+
+	int tw = al_get_bitmap_width(target);
+	int th = al_get_bitmap_height(target);
+
+	int d = 0;
+
+	while (1) {
+		if (al_get_next_event(queue, &event)) {
+			if (event.type == ALLEGRO_EVENT_KEY_DOWN || event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+				break;
+			}
+		}
+
+/*		scanf("%d", &d);*/
+
+		if (d) {
+			switch (d) {
+				case 1:
+					al_draw_bitmap(target, w - tw, 0, 0);
+					al_flip_display();
+					break;
+
+				case 2:
+					al_draw_bitmap(target, w - tw, h - th, 0);
+					al_flip_display();
+					break;
+
+				case 3:
+					al_draw_bitmap(target, 0, h - th, 0);
+					al_flip_display();
+					break;
+
+				default: break;
+			}
+		}
+
+		d++;
+
+		al_get_mouse_state(&state);
+	}
+
+	return 0;
+}
