04a43c82e2d7c128c13e693ab50b081e7b98c595
Will Sember <semberwj@gmail.com>
2013-01-03 09:42:50 -0600

    set calibration program back to fullscreen

--- calibrate.c
+++ calibrate.c
@@ -58,8 +58,8 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-/*	al_set_new_display_flags(ALLEGRO_FULLSCREEN);*/
-	ALLEGRO_DISPLAY *display = al_create_display(800, 1080);
+	al_set_new_display_flags(ALLEGRO_FULLSCREEN);
+	ALLEGRO_DISPLAY *display = al_create_display(1920, 1080);
 
 	if (!display) {
 		printf("could not create display\n");
